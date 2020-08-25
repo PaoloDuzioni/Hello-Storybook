a1154a7fa61e1f408efc87a8900f85f91dceab41
Paolo Duzioni <paolo.duzioni@gmail.com>
2020-08-25 17:28:01 +0200

    fix: removed doc addon already included in sb 6

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,6 @@ module.exports = {
         '../src/**/*.stories.@(js|jsx|ts|tsx)',
     ],
     addons: [
-        '@storybook/addon-docs',
         '@storybook/addon-links',
         '@storybook/addon-essentials',
         '@storybook/preset-create-react-app',
